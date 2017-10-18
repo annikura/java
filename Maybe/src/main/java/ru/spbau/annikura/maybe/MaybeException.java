77327fc48a04c81c296422c86eaf5fc352ccf73e
Ann Shvetsova <annikura@yandex.ru>
2017-10-18 07:04:52 +0300

    Adding docs to MaybeException class

--- Maybe/src/main/java/ru/spbau/annikura/maybe/MaybeException.java
+++ Maybe/src/main/java/ru/spbau/annikura/maybe/MaybeException.java
@@ -1,6 +1,13 @@
 package ru.spbau.annikura.maybe;
 
+/**
+ * Maybe exception class. Will be thrown in case of attempting to access data in Maybe storing nothing.
+ */
 public class MaybeException extends Exception {
+    /**
+     * Constructs an exception with message
+     * @param message
+     */
     public  MaybeException(String message) {
         super(message);
     }
