4049a9658a3ce39e4a81afc651fb0b29871930d6
Ann Shvetsova <annikura@yandex.ru>
2017-11-08 17:08:29 +0300

    (Predicate + collestions) tests

--- FP/src/test/java/ru/spbau/annikura/collections/CollectionsTest.java
+++ FP/src/test/java/ru/spbau/annikura/collections/CollectionsTest.java
@@ -21,6 +21,15 @@ public class CollectionsTest {
     }
 
     @Test
+    public void emptyMap() throws Exception {
+        ArrayList<Integer> arr = new ArrayList<>();
+        List<Integer> funcRes = Collections.map(arr, (num -> num * num));
+        List<Integer> expected = new ArrayList<>();
+        assertEquals(expected, funcRes);
+    }
+
+
+    @Test
     public void filter() throws Exception {
         ArrayList<Integer> arr = new ArrayList<>();
         arr.add(5); arr.add(8); arr.add(19); arr.add(7); arr.add(4); arr.add(11);
@@ -30,8 +39,16 @@ public class CollectionsTest {
             if (el % 2 == 1)
             expected.add(el);
         assertEquals(expected, funcRes);
-
     }
+
+    @Test
+    public void emptyFilter() throws Exception {
+        ArrayList<Integer> arr = new ArrayList<>();
+        List<Integer> funcRes = Collections.filter(arr, num -> true);
+        List<Integer> expected = new ArrayList<>();
+        assertEquals(expected, funcRes);
+    }
+
 
     @Test
     public void takeWhile() throws Exception {
@@ -45,6 +62,14 @@ public class CollectionsTest {
                 expected.add(el);
             else
                 break;
+        assertEquals(expected, funcRes);
+    }
+
+    @Test
+    public void emptyTakeWhile() throws Exception {
+        ArrayList<Integer> arr = new ArrayList<>();
+        List<Integer> funcRes = Collections.takeWhile(arr, num -> true);
+        List<Integer> expected = new ArrayList<>();
         assertEquals(expected, funcRes);
     }
 
@@ -64,11 +89,26 @@ public class CollectionsTest {
     }
 
     @Test
+    public void emptyTakeUntil() throws Exception {
+        ArrayList<Integer> arr = new ArrayList<>();
+        List<Integer> funcRes = Collections.takeUntil(arr, num -> true);
+        List<Integer> expected = new ArrayList<>();
+        assertEquals(expected, funcRes);
+    }
+
+    @Test
     public void foldR() throws Exception {
         ArrayList<Integer> arr = new ArrayList<>();
         arr.add(5); arr.add(8); arr.add(19); arr.add(7); arr.add(4); arr.add(11);
         String funcRes = Collections.foldR(arr, (num, str) -> Integer.toString(num) + str, "x");
         assertEquals("58197411x", funcRes);
+    }
+
+    @Test
+    public void emptyFoldR() throws Exception {
+        ArrayList<Integer> arr = new ArrayList<>();
+        String funcRes = Collections.foldR(arr, (num, str) -> "", "Empty");
+        assertEquals("Empty", funcRes);
     }
 
     @Test
@@ -79,4 +119,10 @@ public class CollectionsTest {
         assertEquals("x11471985", funcRes);
     }
 
+    @Test
+    public void emptyFoldL() throws Exception {
+        ArrayList<Integer> arr = new ArrayList<>();
+        String funcRes = Collections.foldR(arr, (str, num) -> "", "Empty");
+        assertEquals("Empty", funcRes);
+    }
 }
---
+++ FP/src/test/java/ru/spbau/annikura/function/PredicateTest.java
@@ -0,0 +1,113 @@
+package ru.spbau.annikura.function;
+
+import org.junit.Test;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class PredicateTest {
+    @Test
+    public void simpleOr() throws Exception {
+        Predicate<Integer> isOdd = num -> num % 2 == 1;
+        Predicate<Integer> squareLess = num -> num * num < 100;
+        Predicate<Integer> orComp = isOdd.or(squareLess);
+        assertEquals(true, orComp.apply(11));
+        assertEquals(true, orComp.apply(3));
+        assertEquals(true, orComp.apply(6));
+        assertEquals(false, orComp.apply(12));
+        assertEquals(false, orComp.apply(10));
+    }
+
+    @Test
+    public void randomOr() throws Exception {
+        Predicate<Integer> isOdd = num -> num % 2 == 1;
+        Predicate<Integer> squareLess = num -> num * num < 100000;
+        Predicate<Integer> orComp = isOdd.or(squareLess);
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            int num = random.nextInt();
+            assertEquals(isOdd.apply(num) || squareLess.apply(num), orComp.apply(num));
+        }
+    }
+
+    @Test
+    public void lazyOr() throws Exception {
+        Predicate<Integer> isOdd = num -> num % 2 == 1;
+        Predicate<Integer> squareLess = arg -> {
+            assertNotEquals(1, arg % 2);
+            return arg * arg < 1000000;
+        };
+        Predicate<Integer> orComp = isOdd.or(squareLess);
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            int num = random.nextInt();
+            assertEquals(isOdd.apply(num) || squareLess.apply(num), orComp.apply(num));
+        }
+    }
+
+
+    @Test
+    public void simpleAnd() throws Exception {
+        Predicate<Integer> idOdd = num -> num % 2 == 1;
+        Predicate<Integer> squareLess = num -> num * num < 100;
+        Predicate<Integer> andComp = idOdd.and(squareLess);
+        assertEquals(false, andComp.apply(11));
+        assertEquals(true, andComp.apply(3));
+        assertEquals(true, andComp.apply(9));
+        assertEquals(false, andComp.apply(6));
+        assertEquals(false, andComp.apply(12));
+        assertEquals(false, andComp.apply(10));
+    }
+
+
+    @Test
+    public void lazyAnd() throws Exception {
+        Predicate<Integer> isOdd = num -> num % 2 == 1;
+        Predicate<Integer> squareLess = arg -> {
+            assertEquals(1, arg % 2);
+            return arg * arg < 1000000;
+        };
+        Predicate<Integer> andComp = isOdd.and(squareLess);
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            int num = random.nextInt();
+            assertEquals(isOdd.apply(num) && squareLess.apply(num), andComp.apply(num));
+        }
+    }
+
+    @Test
+    public void randomAnd() throws Exception {
+        Predicate<Integer> isOdd = num -> num % 2 == 1;
+        Predicate<Integer> squareLess = num -> num * num < 100000;
+        Predicate<Integer> andComp = isOdd.and(squareLess);
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            int num = random.nextInt();
+            assertEquals(isOdd.apply(num) && squareLess.apply(num), andComp.apply(num));
+        }
+    }
+
+    @Test
+    public void simpleNot() throws Exception {
+        Predicate<Integer> isOdd = num -> num % 2 == 1;
+        Predicate<Integer> isEven = isOdd.not();
+        assertEquals(false, isEven.apply(7));
+        assertEquals(false, isEven.apply(29));
+        assertEquals(false, isEven.apply(24342131));
+        assertEquals(true, isEven.apply(24342132));
+        assertEquals(true, isEven.apply(0));
+        assertEquals(true, isEven.apply(18));
+    }
+
+    @Test
+    public void randomNot() throws Exception {
+        Predicate<Integer> squareLess = num -> num * num < 100000;
+        Predicate<Integer> squareMore = squareLess.not();
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            int num = random.nextInt();
+            assertEquals(!squareLess.apply(num) , squareMore.apply(num));
+        }
+    }
+
+}
