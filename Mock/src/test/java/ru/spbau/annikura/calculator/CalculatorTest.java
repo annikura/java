5409520c27784246fce309dd15773e9d2db17411
Anna Shvetsova <annikura@yandex.ru>
2017-12-05 09:27:42 +0300

    Adding fake stack

--- Mock/src/test/java/ru/spbau/annikura/calculator/CalculatorTest.java
+++ Mock/src/test/java/ru/spbau/annikura/calculator/CalculatorTest.java
@@ -3,14 +3,45 @@ package ru.spbau.annikura.calculator;
 import org.junit.Test;
 import ru.spbau.annikura.stack.MyStack;
 
-import java.util.Stack;
-
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class CalculatorTest {
     @Test
     public void createInstance() {
-        Calculator calculator = new Calculator(mock(MyStack.class));
+        FakeStack fake = new FakeStack();
+        Calculator calculator = new Calculator(spy(fake));
+    }
+}
+
+class FakeStack extends MyStack<ExpressionToken> {
+    private int size = 0;
+    private ExpressionToken[] stack = new ExpressionToken[100];
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public ExpressionToken pop() {
+        assert size > 0;
+        return stack[--size];
+    }
+
+    @Override
+    public ExpressionToken peek() {
+        assert size > 0;
+        return stack[size - 1];
+    }
+
+    @Override
+    public ExpressionToken push(ExpressionToken expressionToken) {
+        return (stack[size++] = expressionToken);
+    }
+
+    @Override
+    public boolean empty() {
+        return size == 0;
     }
 }
