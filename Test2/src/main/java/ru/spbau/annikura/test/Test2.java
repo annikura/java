fe4732f23cfc7d166cb5760c9edc4a916e0a2bc6
Anna Shvetsova <annikura@yandex.ru>
2017-12-13 14:11:00 +0300

    init commit

---
+++ Test2/src/main/java/ru/spbau/annikura/test/Test2.java
@@ -0,0 +1,7 @@
+package ru.spbau.annikura.test;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class Test2 {
+}
---
+++ Test2/src/test/java/ru/spbau/annikura/test/Test2Test.java
@@ -0,0 +1,7 @@
+package ru.spbau.annikura.test;
+
+import static org.junit.Assert.*;
+
+public class Test2Test {
+
+}
