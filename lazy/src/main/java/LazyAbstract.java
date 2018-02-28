3c476c4cddf901d5c7b0c5676c961042e7d875e0
annkura <annikura@yandex.ru>
2018-02-28 15:45:47 +0300

    Init commit: Lazy

---
+++ lazy/src/main/java/LazyAbstract.java
@@ -0,0 +1,21 @@
+import com.sun.istack.internal.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
+
+/**
+ * Stored data abstraction.
+ */
+abstract class LazyAbstract<T> implements Lazy<T> {
+    Supplier<T> supplier;
+    List<T> object = Collections.emptyList();
+
+    /**
+     * Saves supplier to call it when SimpleLazy.get is called.
+     * @param supplier stored supplier
+     */
+    LazyAbstract(@NotNull final Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+}
---
+++ lazy/src/test/java/TestUtils.java
@@ -0,0 +1,32 @@
+import java.util.function.Supplier;
+
+public class TestUtils {
+    public static class LifeMeaningSupplier implements Supplier<Integer> {
+        @Override
+        public Integer get() {
+            return 42;
+        }
+    }
+    public static class CountingSupplier implements Supplier<Integer> {
+        int cnt = 0;
+
+        @Override
+        public Integer get() {
+            cnt++;
+            return cnt;
+        }
+    }
+
+    public static class SlowCountingSupplier implements Supplier<Integer> {
+        int cnt = 0;
+
+        @Override
+        public Integer get() {
+            try {
+                Thread.sleep(1000);
+            } catch(InterruptedException ignored) {}
+            cnt++;
+            return cnt;
+        }
+    }
+}
