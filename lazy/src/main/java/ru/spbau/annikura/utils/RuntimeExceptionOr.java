02b72171d2abd5e53026f630e190ebd164bae7b5
Ann <annikura@yandex.ru>
2018-03-09 01:17:14 +0300

    Added documentation for RuntimeExceprionOr

--- lazy/src/main/java/ru/spbau/annikura/utils/RuntimeExceptionOr.java
+++ lazy/src/main/java/ru/spbau/annikura/utils/RuntimeExceptionOr.java
@@ -1,28 +1,47 @@
 package ru.spbau.annikura.utils;
 
+/**
+ * Resource or runtime exception holder.
+ * @param <T> type of the resource
+ */
 public class RuntimeExceptionOr<T> {
     private final RuntimeException exception;
     private final T resource;
 
+    /**
+     * Constructs holder with exception
+     * @param exception exception that will be held
+     */
     public RuntimeExceptionOr(RuntimeException exception) {
         this.exception = exception;
         resource = null;
     }
 
+    /**
+     * Constructs holder with resource
+     * @param resource resource that will be held
+     */
     public RuntimeExceptionOr(T resource) {
         this.resource = resource;
         exception = null;
     }
 
-
     public boolean isException() {
         return exception != null;
     }
 
+    /**
+     * Exception getter
+     * @return exception if present, null otherwise
+     */
     public RuntimeException getException() {
         return exception;
     }
 
+    /**
+     * Throws stored exception if presented, gives stored resource otherwise
+     * @return resource
+     */
     public T getResource() {
         if (isException()) {
             throw exception;
