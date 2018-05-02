404a335a0067ea6b08add1b5a7724b3c76c53eae
Ann <annikura@yandex.ru>
2018-05-02 06:16:18 +0300

    Added documentation for SocketIOImpl and FileDataRunnable

--- FTP/src/main/java/FileDataRunnable.java
+++ FTP/src/main/java/FileDataRunnable.java
@@ -6,16 +6,49 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.logging.Logger;
 
+/**
+ * Runnable for client-server interaction by FileData protocol.
+ */
+
 class FileDataRunnable implements Runnable {
     private final SocketIO io;
+
+    /**
+     * Creates new FileDataRunnable that uses given socket for interaction with the client.
+     * @param socket a socket to interact with new client.
+     * @throws IOException if an error occurred while connecting to the client.
+     */
     FileDataRunnable(@NotNull Socket socket) throws IOException {
         this(new SocketIOImpl(socket.getInputStream(), socket.getOutputStream()));
     }
 
-    private FileDataRunnable(@NotNull SocketIO io) {
+    /**
+     * Creates new FileDataRunnable that uses given io instance for interaction with the client.
+     * @param io a SocketIO instance to interact with a new client.
+     */
+    FileDataRunnable(@NotNull SocketIO io) {
         this.io = io;
     }
 
+    /**
+     * Method implementing server-client interaction.
+     *
+     * Supported requests:
+     *  File content request -- given a path to a file, returns its content as bytes[]. If no such a file exists, returns empty array.
+     *  File listing request -- given a path to the directory, returns files located directly in the given directory as list of pairs:
+     *      (name of file, whether it is a directory). If no such directory exists, returns empty list.
+     *  Exit request -- returns from this method.
+     * Communication protocol:
+     *  File content request:
+     *      Client request format expected: (2 :Int) [(path length :Int) (path :String)]
+     *      Response format: (file size :Int) (content :byte[])
+     *
+     *  File listing request:
+     *      Client request format expected: (1 :Int) [(path length :Int) (path :String)]
+     *      Response format: (number of files :Int) [(file name length :Int) (file name :String) (is directory :boolean)]*
+     *  Exit request:
+     *      Client request format expected: (3: Int)
+     */
     public void run() {
         Logger.getAnonymousLogger().info("New thread has started");
         try {
@@ -47,8 +80,14 @@ class FileDataRunnable implements Runnable {
         Logger.getAnonymousLogger().info("Shutting down server thread...");
     }
 
+    /**
+     * Given the name of the file, returns its content as bytes[].
+     * @param filename name of the file to get the contents of.
+     * @return contents of the file.
+     * @throws IOException if an error occurred while reading the file.
+     */
     @NotNull
-    private byte[] getFile(@NotNull String filename) throws IOException {
+    static byte[] getFile(@NotNull String filename) throws IOException {
         File file = new File(filename);
         if (!file.exists()) {
             return new byte[0];
@@ -59,8 +98,13 @@ class FileDataRunnable implements Runnable {
         return result;
     }
 
+    /**
+     * Given the name of the directory, returns list of files located in it.
+     * @param dir path to a directory which content will be listed.
+     * @return list of ServerFile instances, describing files in the given directory.
+     */
     @NotNull
-    private static ServerFile[] getDirContent(@NotNull String dir) {
+    static ServerFile[] getDirContent(@NotNull String dir) {
         File fileDir = new File(dir);
         File[] dirContent = fileDir.listFiles();
         if (!fileDir.exists() || dirContent == null) {
--- FTP/src/main/java/SocketIOImpl.java
+++ FTP/src/main/java/SocketIOImpl.java
@@ -2,14 +2,18 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 
+/**
+ * SocketIO implementation which uses Data*Streams to provide communication between client and server.
+ * An important fact is that this implementation flushes out stream after each write.
+ */
 class SocketIOImpl implements SocketIO {
     private final DataOutputStream out;
     private final DataInputStream in;
 
     /**
-     * Creates So
-     * @param in
-     * @param out
+     * Creates SocketIO instance using given in and out streams as input and output correspondingly
+     * @param in input stream
+     * @param out output stream
      */
     SocketIOImpl(@NotNull final InputStream in, @NotNull final OutputStream out) {
         this.in = new DataInputStream(new BufferedInputStream(in));
@@ -20,31 +24,63 @@ class SocketIOImpl implements SocketIO {
         out.flush();
     }
 
+    /**
+     * Writes bytes to the out stream and flushes it.
+     * @param bytes bytes to be written into the socket
+     * @throws IOException if an error occurred while writing into the stream.
+     */
     public void writeBytes(@NotNull byte[] bytes) throws IOException {
         out.write(bytes);
         afterWrite();
     }
 
+
+    /**
+     * Writes integer to the out stream and flushes it
+     * @param number integer to be written into the socket
+     * @throws IOException if an error occurred while writing into the stream.
+     */
     public void writeInt(int number) throws IOException {
         out.writeInt(number);
         afterWrite();
     }
 
+    /**
+     * Writes string to the out stream and flushes it
+     * @param string string to be written into the socket
+     * @throws IOException if an error occurred while writing into the stream.
+     */
     public void writeString(@NotNull String string) throws IOException {
         out.writeChars(string);
         afterWrite();
     }
 
+    /**
+     * Writes long to the out stream and flushes it
+     * @param number long to be written into the socket
+     * @throws IOException if an error occurred while writing into the stream.
+     */
     public void writeLong(long number) throws IOException {
         out.writeLong(number);
         afterWrite();
     }
 
+    /**
+     * Writes boolean to the out stream and flushes it
+     * @param bool boolean to be written into the socket
+     * @throws IOException if an error occurred while writing into the stream.
+     */
     public void writeBoolean(boolean bool) throws IOException {
         out.writeBoolean(bool);
         afterWrite();
     }
 
+    /**
+     * Reads given number of bytes from the stream.
+     * @param length number of bytes
+     * @return array of bytes that was read.
+     * @throws IOException if an error occurred while reading from the stream.
+     */
     @NotNull
     public byte[] readBytes(long length) throws IOException {
         byte[] bytes = new byte[(int) length];
@@ -52,10 +88,21 @@ class SocketIOImpl implements SocketIO {
         return bytes;
     }
 
+    /**
+     * Reads given int stream.
+     * @return int that was read
+     * @throws IOException if an error occurred while reading from the stream.
+     */
     public int readInt() throws IOException {
         return in.readInt();
     }
 
+    /**
+     * Reads string of the given length from the stream.
+     * @param length string length
+     * @return string that was read
+     * @throws IOException if an error occurred while reading from the stream.
+     */
     @NotNull
     public String readString(int length) throws IOException {
         char[] chars = new char[length];
@@ -65,14 +112,28 @@ class SocketIOImpl implements SocketIO {
         return String.copyValueOf(chars);
     }
 
+    /**
+     * Reads long from the stream.
+     * @return long that was read
+     * @throws IOException if an error occurred while reading from the stream.
+     */
     public long readLong() throws IOException {
         return in.readLong();
     }
 
+    /**
+     * Reads boolean from the string.
+     * @return boolean that was read
+     * @throws IOException if an error occurred while reading from the stream.
+     */
     public boolean readBoolean() throws IOException {
         return in.readBoolean();
     }
 
+    /**
+     * Closes communication streams.
+     * @throws IOException if an error occurred while closing the streams.
+     */
     public void close() throws IOException {
         in.close();
         out.close();
