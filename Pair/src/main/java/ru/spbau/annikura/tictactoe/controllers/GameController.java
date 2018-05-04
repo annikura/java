b7b54d6ed44002bb0d05b1c0d97403b37b5ef347
Ann <annikura@yandex.ru>
2018-05-04 18:34:04 +0300

    Added javadocs to GameController

--- Pair/src/main/java/ru/spbau/annikura/tictactoe/controllers/GameController.java
+++ Pair/src/main/java/ru/spbau/annikura/tictactoe/controllers/GameController.java
@@ -2,6 +2,9 @@ package ru.spbau.annikura.tictactoe.controllers;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Pair game controller.
+ */
 public class GameController {
     private final GameField board;
     private GameStatus status = GameStatus.CHOOSE_FIRST_TILE;
@@ -12,12 +15,21 @@ public class GameController {
         return status;
     }
 
+    /**
+     * Creates new game with given board size.
+     * @param size size of the new board
+     */
     public GameController(int size) {
         assert size % 2 == 0;
         this.size = size;
         board = GameField.generate(size);
     }
 
+    /**
+     * Processes the current state of the board and if it matches a match-try state,
+     * changes the state of the highlighted pair according to the status.
+     * @return new board state.
+     */
     public GameField makeDecision() {
         if (!status.equals(GameStatus.WRONG_MATCH) && !status.equals(GameStatus.SUCCESSFUL_MATCH)) {
             return board;
@@ -41,7 +53,14 @@ public class GameController {
         status = notEnd ? GameStatus.CHOOSE_FIRST_TILE : GameStatus.THE_END;
         return board;
     }
-    
+
+    /**
+     * Makes move, trying to highlight a cell on the given position.
+     * If this cell already disabled of highlighted, nothing happens.
+     * @param i cell column.
+     * @param j cell row.
+     * @return new state of the board.
+     */
     public GameField makeMove(int i, int j) {
         if (board.get(i, j).isDisabled || board.get(i, j).isHighlighted) {
             return board;
@@ -64,6 +83,9 @@ public class GameController {
         return board;
     }
 
+    /**
+     * Class for indicating the state of the Pair game.
+     */
     public enum GameStatus {
         CHOOSE_FIRST_TILE,
         CHOOSE_SECOND_TILE,
@@ -72,23 +94,41 @@ public class GameController {
         WRONG_MATCH
     }
 
+    /**
+     * Game board cell class.
+     */
     public static class Cell {
         private final int value;
         private boolean isDisabled = false;
         private boolean isHighlighted = false;
 
+        /**
+         * Creates an enabled not highlighted cell holding a given value.
+         * @param value stored value.
+         */
         public Cell(int value) {
             this.value = value;
         }
 
+        /**
+         * Disables the cell.
+         */
         void disable() {
             isDisabled = true;
         }
 
+        /**
+         * Sets cell highlighting to the given one.
+         * @param highlight highlighting to be set.
+         */
         void setHighlight(boolean highlight) {
             isHighlighted = highlight;
         }
 
+        /**
+         * Processes value to a string format depending on the button state.
+         * @return value if cell is highlighted, empty string otherwise.
+         */
         @NotNull
         public String getValue() {
             if (isHighlighted) {
@@ -106,17 +146,35 @@ public class GameController {
         }
     }
 
+    /**
+     * Class storing a Pair game board.
+     */
     public static class GameField {
         private Cell[][] board;
 
+        /**
+         * Cell getter.
+         * @param i cell column
+         * @param j cell row
+         * @return cell located on the given position.
+         */
         public Cell get(int i, int j) {
             return board[i][j];
         }
 
+        /**
+         * Creates a board of the given size filled with nulls.
+         * @param size new board size.
+         */
         GameField(int size) {
             board = new Cell[size][size];
         }
 
+        /**
+         * Generates new board of the given size filled with pairs of numbers in range 0..size^2 / 2 distributed randomly on the board.
+         * @param size size of the board.
+         * @return generated board.
+         */
         public static GameField generate(int size) {
             GameField newBoard = new GameField(size);
 
